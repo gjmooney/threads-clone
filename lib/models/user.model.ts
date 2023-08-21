b0f1a8f988a968f93dea74a6c643fc33d4eb1bfa
Greg <gjmooney@asu.edu>
2023-08-21 17:42:37 +0200

    Add mongo config

---
+++ lib/models/user.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  filaments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Filament",
+    },
+  ],
+  onBoarded: {
+    type: Boolean,
+    default: false,
+    communities: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Community",
+      },
+    ],
+  },
+});
+
+// Use existing model OR create new model
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
